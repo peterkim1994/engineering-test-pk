bf3b9787e4f99b16ba20ba1cda064ac97444d2ab
peterkim1994 <peterdkim333@gmail.com>
2026-05-02 14:52:51 +1200

    add constants for item

---
+++ src/GildedRose.Console/Constants/ItemConstants.cs
@@ -0,0 +1,10 @@
+namespace GildedRose.Console.Constants;
+
+public class ItemConstants
+{
+    public const int StandardQualityLimit = 50;
+    
+    public const int MinimumQualityRating = 0;
+    
+    public const int LegendaryCardQualityRating = 80;
+}
