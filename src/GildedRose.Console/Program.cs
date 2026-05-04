2936f6a3955b0f8beb33b974a604aaeaf46416ef
peterkim1994 <peterdkim333@gmail.com>
2026-05-04 20:19:33 +1200

    use new ItemUpdater class
    -updated the Program file to use ItemUpdater
    -deleted old item update logic
    -update Program tests

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿namespace GildedRose.Console;
+﻿using GildedRose.Console.ItemUpdaters;
 
 public class Program
 {
@@ -33,55 +34,11 @@ public class Program
 
     public void UpdateQuality()
     {
-        for (var i = 0; i < Items.Count; i++)
+        var itemUpdater = new ItemUpdater();
+        foreach (var item in Items)
         {
-            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if (Items[i].Quality > 0)
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        Items[i].Quality = Items[i].Quality - 1;
-            }
-            else
-            {
-                if (Items[i].Quality < 50)
-                {
-                    Items[i].Quality = Items[i].Quality + 1;
-
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].SellIn < 11)
-                            if (Items[i].Quality < 50)
-                                Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].SellIn < 6)
-                            if (Items[i].Quality < 50)
-                                Items[i].Quality = Items[i].Quality + 1;
-                    }
-                }
-            }
-
-            if (Items[i].Name != "Sulfuras, Hand of Ragnaros") Items[i].SellIn = Items[i].SellIn - 1;
-
-            if (Items[i].SellIn < 0)
-            {
-                if (Items[i].Name != "Aged Brie")
-                {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].Quality > 0)
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                Items[i].Quality = Items[i].Quality - 1;
-                    }
-                    else
-                    {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50) Items[i].Quality = Items[i].Quality + 1;
-                }
-            }
+            itemUpdater.UpdateItem(item);
+            System.Console.WriteLine($"{item.Name} sellin: {item.SellIn}  quality: {item.Quality}");
         }
     }
 }
--- src/GildedRose.Tests/ProgramTests.cs
+++ src/GildedRose.Tests/ProgramTests.cs
@@ -1,32 +1,75 @@
+using GildedRose.Console;
+using GildedRose.Console.Types;
 using Xunit;
 
 namespace GildedRose.Tests;
 
 public class ProgramTests
 {
-    [Fact] public void UpdateQuality_MixedItemTypes_UpdatesQualityDependingOnItemType()
+    [Fact]
+    public void UpdateQuality_MixedItemTypes_UpdatesQualityDependingOnItemType()
     {
-        Assert.True(true);
+        var app = new Program
+        {
+            Items = new List<Item>
+            {
+                ItemFixtures.DexterityVest(sellIn: 10, quality: 20),
+                ItemFixtures.AgedBrie(sellIn: 10, quality: 20),
+                ItemFixtures.ElixirOfTheMongoose(sellIn: 10, quality: 20),
+                ItemFixtures.Sulfuras(sellIn: 10),
+                ItemFixtures.BackstagePasses(sellIn: 10, quality: 20)
+            }
+        };
+
+        app.UpdateQuality();
+
+        Assert.Equal(19, app.Items[0].Quality);
+        Assert.Equal(9, app.Items[0].SellIn);
+
+        Assert.Equal(21, app.Items[1].Quality);
+        Assert.Equal(9, app.Items[1].SellIn);
+
+        Assert.Equal(19, app.Items[2].Quality);
+        Assert.Equal(9, app.Items[2].SellIn);
+
+        Assert.Equal(ItemConstants.LegendaryCardQualityRating, app.Items[3].Quality);
+        Assert.Equal(10, app.Items[3].SellIn);
+
+        Assert.Equal(22, app.Items[4].Quality);
+        Assert.Equal(9, app.Items[4].SellIn);
     }
-    
-    [Fact] public void UpdateQuality_MixedConjuredItemTypes_UpdatesQualityDependingOnItemType()
+
+    [Fact]
+    public void UpdateQuality_MixedConjuredItemTypes_UpdatesQualityDependingOnItemType()
     {
-        Assert.True(true);
-    }
+        var app = new Program
+        {
+            Items = new List<Item>
+            {
+                ItemFixtures.ConjuredDexterityVest(sellIn: 10, quality: 40),
+                ItemFixtures.ConjuredAgedBrie(sellIn: 10, quality: 40),
+                ItemFixtures.ConjuredElixirOfTheMongoose(sellIn: 10, quality: 40),
+                ItemFixtures.ConjuredSulfuras(sellIn: 10),
+                ItemFixtures.ConjuredBackstagePasses(sellIn: 10, quality: 40)
+            }
+        };
+
+        app.UpdateQuality();
+
+        Assert.Equal(38, app.Items[0].Quality);
+        Assert.Equal(9, app.Items[0].SellIn);
+
+        Assert.Equal(42, app.Items[1].Quality);
+        Assert.Equal(9, app.Items[1].SellIn);
+
+        Assert.Equal(38, app.Items[2].Quality);
+        Assert.Equal(9, app.Items[2].SellIn);
+
+        Assert.Equal(ItemConstants.LegendaryCardQualityRating, app.Items[3].Quality);
+        Assert.Equal(10, app.Items[3].SellIn);
     
-    [Fact] public void UpdateQuality_StandardItemsWithZeroQuality_DoesntDecreaseQuality()
-    {
-        Assert.True(true);
-    }
-    
-    [Fact] public void UpdateQuality_StandardItemsGoingOverSellInDate_DecreasesQualityDoubleTheRate()
-    {
-        Assert.True(true);
-    }
-    
-    [Theory]
-    public void UpdateQuality_FutureEvent_DecreasesQualityAmountBasedOnSellInDate()
-    {
-        Assert.True(true);
+        // conjured tickets do not appreciate twice at twice the rate.
+        Assert.Equal(42, app.Items[4].Quality);
+        Assert.Equal(9, app.Items[4].SellIn);
     }
 }
